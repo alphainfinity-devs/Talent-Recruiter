061052550344061abcd8c308eaf48bf44ce719e8
alphainfinity-devs <rasel.mahmud.swpu@gmail.com>
2022-10-05 02:59:39 +0800

    tailwind installed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-primary">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline text-primary">
+        Hello world! we will start our project
+      </h1>
     </div>
   );
 }
