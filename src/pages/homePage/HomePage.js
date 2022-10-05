9bdd797c33a3dd41b530aaa9c9b9d60ca3a05add
Ahasan Habib Sany <sany.baust@gmail.com>
2022-10-05 18:28:53 +0600

    review done - sany

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HomePageBlogSection from './homePageComponents/HomePageBlogSection';
+import HomePageReviewSection from './homePageComponents/HomePageReviewSection';
 
 const HomePage = () => {
     return (
         <div>
             <HomePageBlogSection/>
+            <HomePageReviewSection/>
         </div>
     );
 };
--- src/pages/homePage/homePageComponents/HomePageReviewSection.js
+++ src/pages/homePage/homePageComponents/HomePageReviewSection.js
@@ -2,9 +2,76 @@ import React from 'react';
 
 const HomePageReviewSection = () => {
     return (
-        <>
-         Review   
-        </>
+        <div className='my-28'>
+            <div className='text-center  mb-14'>
+                <h3 className='text-primary  text-4xl font-bold uppercase py-3'>Client Review</h3>
+                <h2 className='text-xl'>Proident aliquip sit amet culpa id.</h2>
+            </div>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10'>
+            <div className="card lg:max-w-lg bg-base-100 shadow-xl mx-auto">
+                <div className="">
+                    <div className="w-40 block m-auto">
+                        <img className="mask mask-circle"  src="https://placeimg.com/400/225/arch" alt="Shoes" />             
+                        </div>
+                </div>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Review Post - 1</h2>
+                    <p>
+                     Incididunt culpa officia commodo eiusmod esse enim nulla aliquip consequat. Voluptate consequat ex eiusmod ad. Labore occaecat laboris esse ad mollit minim nulla.
+
+                    </p>
+                </div>
+                </div>            
+                <div className="card lg:max-w-lg bg-base-100 shadow-xl mx-auto">
+                <div className="">
+                    <div className="w-40 block m-auto">
+                        <img className="mask mask-circle"  src="https://placeimg.com/400/225/arch" alt="Shoes" />             
+                    </div>
+                </div>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Review Post - 2</h2>
+                    <p>
+                     Incididunt culpa officia commodo eiusmod esse enim nulla aliquip consequat. Voluptate consequat ex eiusmod ad. Labore occaecat laboris esse ad mollit minim nulla.
+
+                    </p>
+                </div>
+                </div>
+                <div className="card lg:max-w-lg bg-base-100 shadow-xl mx-auto">
+                    <div className="">
+                    {/* mask mask-circle */}
+                        <div className="w-40 block m-auto">
+                            <img className="mask mask-circle"  src="https://placeimg.com/400/225/arch" alt="Shoes" />             
+                        </div>
+        
+                    </div>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Review Post - 3</h2>
+                        <p>
+
+                        Incididunt culpa officia commodo eiusmod esse enim nulla aliquip consequat. Voluptate consequat ex eiusmod ad. Labore occaecat laboris esse ad mollit minim nulla.
+
+                        </p>
+                    </div>
+                </div>
+                <div className="card lg:max-w-lg bg-base-100 shadow-xl mx-auto">
+                    <div className="">
+                    {/* mask mask-circle */}
+                        <div className="w-40 block m-auto">
+                            <img className="mask mask-circle"  src="https://placeimg.com/400/225/arch" alt="Shoes" />             
+                        </div>
+        
+                    </div>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Review Post - 3</h2>
+                        <p>
+
+                        Incididunt culpa officia commodo eiusmod esse enim nulla aliquip consequat. Voluptate consequat ex eiusmod ad. Labore occaecat laboris esse ad mollit minim nulla.
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
