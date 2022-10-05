4b28fc6f5290b2ab560c7959e7d93f154d0b8d92
alphainfinity-devs <rasel.mahmud.swpu@gmail.com>
2022-10-06 01:40:41 +0800

    how it work component added

--- src/pages/homePage/homePageComponents/HowItWork.js
+++ src/pages/homePage/homePageComponents/HowItWork.js
@@ -1,6 +1,10 @@
 import React from "react";
 import howIcon from "../../../assets/images/how.svg";
-import {FaUsers} from "react-icons/fa";
+import { FaUsers } from "react-icons/fa";
+import { MdOutlineAppRegistration, MdYoutubeSearchedFor } from "react-icons/md";
+import { FcSearch } from "react-icons/fc";
+import { HiOutlineBadgeCheck } from "react-icons/hi";
+import { TbBadge } from "react-icons/tb";
 
 const HowItWork = () => {
   return (
@@ -13,7 +17,7 @@ const HowItWork = () => {
         />
         How It Works?
       </h1>
-      <p className="w-1/2 mx-auto">
+      <p className="md:w-1/2 lg:w-1/2 p-1 mx-auto">
         TalentRecruiter is a job hunting platform where users can find the
         desired jobs. In this web application, we provide different kinds of
         jobs from multiple companies. Job seekers can post resumes, portfolios,
@@ -21,22 +25,57 @@ const HowItWork = () => {
         recruiters can look through such information when searching for
         candidates to hire.
       </p>
-      <div className="flex justify-between">
-        <div>
-          <div className="card w-96 bg-neutral text-neutral-content">
-                      <div className="card-body items-center text-center">
-                          <FaUsers/>
-              <h2 className="card-title">Create an Account</h2>
-              <p>We are using cookies for no reason.</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Accept</button>
-                <button className="btn btn-ghost">Deny</button>
-              </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 place-items-center scroll-py-56 my-6">
+        <div className="card w-96 bg-neutral text-neutral-content">
+          <div className="card-body items-center text-center">
+            <FaUsers className="h-24 w-24 border p-2 rounded-full" />
+            <h2 className="card-title">Create an Account</h2>
+            <p>
+              Post a job to tell us about your project. We'll quickly match you
+              with the right freelancers find place best.
+            </p>
+            <div className="card-actions justify-center my-2">
+              <button className="btn btn-primary rounded-full">
+                <MdOutlineAppRegistration size={22} className="mr-1" /> Sing Up
+                Now
+              </button>
             </div>
           </div>
         </div>
-        <div>Create an Account</div>
-        <div>Create an Account</div>
+        <div className="card w-96 bg-neutral text-neutral-content">
+          <div className="card-body items-center text-center">
+            <FcSearch className="h-24 w-24" />
+            <h2 className="card-title">Search Jobs</h2>
+            <p>
+              Post a job to tell us about your project. We'll quickly match you
+              with the right freelancers find place best.
+            </p>
+            <div className="card-actions justify-center my-2">
+              <button className="btn btn-primary rounded-full">
+                <MdYoutubeSearchedFor size={22} className="mr-1" /> Search Now
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-neutral text-neutral-content">
+          <div className="card-body items-center text-center">
+            <HiOutlineBadgeCheck
+              color="#1DBF73"
+              className="h-24 w-24 border p-2 rounded-full"
+            />
+            <h2 className="card-title">Apply Job</h2>
+            <p>
+              Post a job to tell us about your project. We'll quickly match you
+              with the right freelancers find place best.
+            </p>
+            <div className="card-actions justify-center my-2">
+              <button className="btn btn-primary rounded-full">
+                <TbBadge size={22} className="mr-1" /> Apply
+                Now
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
