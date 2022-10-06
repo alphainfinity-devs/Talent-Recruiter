362f8b496eb0a5cfff90f08d2cadca8b98ece1b3
anikjoyy <anikjoy.cse@gmail.com>
2022-10-06 11:58:39 +0600

    created featured job section

--- src/pages/homePage/homePageComponents/HomePageComponents.js
+++ src/pages/homePage/homePageComponents/HomePageComponents.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Categories from './Categories/Categories';
+import FeatureJobs from './FeatureJobs/FeatureJobs';
 
 const HomePageComponents = () => {
   return (
     <div>
       <Categories />
+      <FeatureJobs />
     </div>
   );
 };
