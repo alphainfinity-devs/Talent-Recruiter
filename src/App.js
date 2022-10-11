ded17f5d4764b449b0d3ec6c20f08905ecd3e791
anikjoyy <anikjoy.cse@gmail.com>
2022-10-11 14:16:56 +0600

    modified some file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from "react";
-import { ToastContainer } from "react-toastify";
-import HomePage from "./pages/homePage/HomePage";
-import Footer from "./globalComponents/Footer";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./globalComponents/Navbar";
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import HomePage from './pages/homePage/HomePage';
+import Footer from './globalComponents/Footer';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './globalComponents/Navbar';
+
 function App() {
   return (
     <>
       <>
         <Navbar />
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path='/' element={<HomePage />} />
         </Routes>
         <Footer />
       </>
       {/* for toastify popup */}
       <ToastContainer
-        position="top-center"
+        position='top-center'
         autoClose={5000}
         hideProgressBar={false}
         newestOnTop={false}
--- src/globalComponents/Navbar.js
+++ src/globalComponents/Navbar.js
@@ -1,33 +1,33 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const Navbar = () => {
   const [checkbox, setCheckbox] = useState(false);
 
   return (
     <header>
-      <div className="flex justify-between py-3 items-center container mx-auto px-5">
+      <div className='flex justify-between py-3 items-center container mx-auto px-5'>
         {/*.......Website Logo........*/}
         <div>
-          <h2 className="text-4xl font-bold text-primary">Telent.</h2>
+          <h2 className='text-4xl font-bold text-primary'>Telent.</h2>
         </div>
 
         {/*.......md and lg navbar start.......*/}
-        <div className="hidden md:block">
-          <div className="flex items-center justify-between sm:gap-10">
+        <div className='hidden md:block'>
+          <div className='flex items-center justify-between sm:gap-10'>
             <div>
-              <ul className="flex md:gap-3 lg:gap-6">
-                <li className="font-bold">Home</li>
-                <li className="font-bold">About</li>
-                <li className="font-bold">Reviews</li>
-                <li className="font-bold">Blogs</li>
-                <li className="font-bold">Contact</li>
+              <ul className='flex md:gap-3 lg:gap-6'>
+                <li className='font-bold'>Home</li>
+                <li className='font-bold'>About</li>
+                <li className='font-bold'>Reviews</li>
+                <li className='font-bold'>Blogs</li>
+                <li className='font-bold'>Contact</li>
               </ul>
             </div>
-            <div className="flex gap-2">
-              <button className="px-4 bg-white hover:bg-primary text-primary border-primary hover:border-primary hover:text-white font-bold border-2 hover:border-2">
+            <div className='flex gap-2'>
+              <button className='px-4 bg-white hover:bg-primary text-primary border-primary hover:border-primary hover:text-white font-bold border-2 hover:border-2'>
                 Login
               </button>
-              <button className="bg-primary hover:bg-white text-white border-primary hover:border-primary hover:text-primary font-bold px-4 py-2 border-2 hover:border-2">
+              <button className='bg-primary hover:bg-white text-white border-primary hover:border-primary hover:text-primary font-bold px-4 py-2 border-2 hover:border-2'>
                 Register
               </button>
             </div>
@@ -36,40 +36,40 @@ const Navbar = () => {
         {/*........md and lg navbar start.........*/}
 
         {/*.........Mobile navbar start..........*/}
-        <label className="btn bg-white hover:bg-white px-3 swap swap-rotate border-2 md:hidden">
-          <input type="checkbox" onClick={() => setCheckbox(!checkbox)} />
+        <label className='btn bg-white hover:bg-white px-3 swap swap-rotate border-2 md:hidden'>
+          <input type='checkbox' onClick={() => setCheckbox(!checkbox)} />
           <svg
-            className="swap-off"
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 512 512"
+            className='swap-off'
+            xmlns='http://www.w3.org/2000/svg'
+            width='32'
+            height='32'
+            viewBox='0 0 512 512'
           >
-            <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+            <path d='M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z' />
           </svg>
           <svg
-            className="swap-on"
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 512 512"
+            className='swap-on'
+            xmlns='http://www.w3.org/2000/svg'
+            width='32'
+            height='32'
+            viewBox='0 0 512 512'
           >
-            <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+            <polygon points='400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49' />
           </svg>
         </label>
 
         {checkbox && (
-          <div className="absolute top-20 inset-x-4 z-10 md:hidden">
-            <ul className="menu bg-base-100 p-5 shadow-lg rounded-box border-2 w-full gap-y-3">
-              <li className="font-bold">Home</li>
-              <li className="font-bold">About</li>
-              <li className="font-bold">Reviews</li>
-              <li className="font-bold">Blogs</li>
-              <li className="font-bold">Contact</li>
+          <div className='absolute top-20 inset-x-4 z-10 md:hidden'>
+            <ul className='menu bg-base-100 p-5 shadow-lg rounded-box border-2 w-full gap-y-3'>
+              <li className='font-bold'>Home</li>
+              <li className='font-bold'>About</li>
+              <li className='font-bold'>Reviews</li>
+              <li className='font-bold'>Blogs</li>
+              <li className='font-bold'>Contact</li>
 
-              <div className="flex gap-2">
-                <button className="btn btn-primary">Login</button>
-                <button className="btn btn-primary">Register</button>
+              <div className='flex gap-2'>
+                <button className='btn btn-primary'>Login</button>
+                <button className='btn btn-primary'>Register</button>
               </div>
             </ul>
           </div>
