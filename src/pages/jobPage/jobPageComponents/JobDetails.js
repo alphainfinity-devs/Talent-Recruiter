6b3e535b8a8d8965fe77de56eb072850ae24d528
Ahasan Habib Sany <sany.baust@gmail.com>
2022-10-19 15:19:44 +0600

    job card updated

--- src/pages/jobPage/jobPageComponents/JobDetails.js
+++ src/pages/jobPage/jobPageComponents/JobDetails.js
@@ -25,45 +25,34 @@ const JobDetails = () => {
             <>
             <PageTitleBanner title={job?.title} />
               <div className="shadow-lg p-4 mb-4 border">
-                <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-3">
+                <div className="grid grid-flow-row-dense sm:grid-cols-1 md:grid-cols-2 gap-3">
                   {/* .......compani Logo...... */}
-                  <div className="flex justify-center md:justify-start items-center bg-secondary rounded">
+                  {/* <div className="flex justify-center md:justify-start items-center bg-secondary rounded">
                     <img
                       src="https://templates.envytheme.com/jubi/default/assets/images/hot-jobs/hot-jobs-6.png"
                       className="w-24 p-1"
                       alt=""
                     />
-                  </div>
+                  </div> */}
 
                   {/* .......Basic job information........ */}
                   <div className="grid justify-items-center lg:justify-items-start">
-                    <p className="text-lg font-bold">{job.title}</p>
-                    <p className="text-sm">www.youtube.com</p>
-                    <p className="font-bold">Vacancy: 01</p>
-                    <div className="flex flex-row gap-4 mt-2 mb-2">
-                      {/* <p className="text-sm font-light flex flex-row gap-1 items-center">
-                    <FiBox />
-                    Hexagon
-                  </p> */}
-                      <p className="text-sm font-light flex flex-row gap-1 items-center">
-                        <FaMapMarkerAlt />
-                        {job.address}
-                      </p>
-                      <p className="text-sm font-light flex flex-row gap-1 items-center">
-                        <AiOutlineFieldTime />
-                        {job.level}
-                      </p>
-                    </div>
+                    <p className="text-lg font-bold ">{job?.title}</p>
+                    <p className="text-sm">{job?.company_link}</p>
+                    <p className="font-bold">Vacancy: {job?.vacancy}</p>
+                    <p className="text-base font-bold">
+                      Deadline: {job?.dead_line}
+                    </p>
                   </div>
 
                   {/* .........apply button and deadline....... */}
-                  <div className="flex flex-col gap-2">
+                  <div className="flex justify-center  md:justify-end gap-1">
                     <button className="btn rounded-none text-white bg-primary hover:bg-accent">
                       Apply Now
                     </button>
-                    <p className="text-base font-bold">
-                      Deadline: Dec 01, 2022
-                    </p>
+                    <button className="btn rounded-none text-white bg-warning hover:bg-accent">
+                      Save Now
+                    </button>
                   </div>
                 </div>
               </div>
@@ -73,7 +62,7 @@ const JobDetails = () => {
                 <p className="text-2xl font-semibold text-accent">
                   Job Description
                 </p>
-                <p className="py-4">{job.description}</p>
+                <p className="py-4">{job?.description}</p>
 
                 {/*...........Education & Experience............*/}
                 <p className="text-2xl font-semibold text-accent py-4">
@@ -135,32 +124,21 @@ const JobDetails = () => {
             </h2>
 
             <div className="flex flex-row justify-start items-center gap-5 px-4 py-2">
-              <div className="text-3xl text-primary">
+              <div className="text-1xl text-primary">
                 <FaMoneyCheckAlt />
               </div>
               <div className="grid justify-items-center lg:justify-items-start">
-                <p className="text-lg font-bold">Salary</p>
-                <p className="text-sm font-light">£12,000 - £25,000</p>
+                <p className="text-lg font-bold">Salary : <span className="text-sm font-light">{job?.salary}</span></p>
               </div>
             </div>
 
             <div className="flex flex-row justify-start items-center gap-5 px-4 py-2">
-              <div className="text-3xl text-primary">
+              <div className="text-1xl text-primary">
                 <FaMapMarkerAlt />
               </div>
               <div className="grid justify-items-center lg:justify-items-start">
-                <p className="text-lg font-bold">Location</p>
-                <p className="text-sm font-light">New York City</p>
-              </div>
-            </div>
-
-            <div className="flex flex-row justify-start items-center gap-5 px-4 py-2">
-              <div className="text-3xl text-primary">
-                <BsGenderAmbiguous />
-              </div>
-              <div className="grid justify-items-center lg:justify-items-start">
-                <p className="text-lg font-bold">Gender</p>
-                <p className="text-sm font-light">Any</p>
+                <p className="text-lg font-bold">Location : <span className="text-sm font-light">{job?.address}</span>
+                </p>
               </div>
             </div>
 
@@ -169,8 +147,7 @@ const JobDetails = () => {
                 <MdWorkOutline />
               </div>
               <div className="grid justify-items-center lg:justify-items-start">
-                <p className="text-lg font-bold">Job Type</p>
-                <p className="text-sm font-light">Full Time</p>
+                <p className="text-lg font-bold">Job Type : <span className="text-sm font-light">{job?.type} </span></p>
               </div>
             </div>
 
@@ -179,8 +156,7 @@ const JobDetails = () => {
                 <MdCastForEducation />
               </div>
               <div className="grid justify-items-center lg:justify-items-start">
-                <p className="text-lg font-bold">Qualification</p>
-                <p className="text-sm font-light">Mba</p>
+                <p className="text-lg font-bold">Qualification : <span className="text-sm font-light">Mba</span></p>
               </div>
             </div>
 
@@ -189,8 +165,7 @@ const JobDetails = () => {
                 <BiNotepad />
               </div>
               <div className="grid justify-items-center lg:justify-items-start">
-                <p className="text-lg font-bold">Experience</p>
-                <p className="text-sm font-light">2 to 3 year</p>
+                <p className="text-lg font-bold">Experience : <span className="text-sm font-light">{job?.experience}</span></p>
               </div>
             </div>
 
