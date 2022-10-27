734856d0a82bb4e47a0041ffb4b418dda35b4f4f
nayim-hossen <nayimh2017@gmail.com>
2022-10-28 00:45:44 +0600

    logo image

--- src/pages/jobPage/jobPageComponents/JobListCard.js
+++ src/pages/jobPage/jobPageComponents/JobListCard.js
@@ -13,7 +13,7 @@ const JobListCard = (job) => {
     <Link to={link}>
       <div className="flex justify-start items-center bg-base-100 shadow hover:shadow-lg gap-4 px-5 py-6 cursor-pointer hover:border-primary border-white border hover:-translate-y-2 duration-700">
         <div className="bg-[tomato] p-4 py-7 text-white text-3xl">
-          {companyLogo}
+          <img src={companyLogo} alt="compnay logo" />
         </div>
         <div className="">
           <h2 className="font-bold text-xl md:text-2xl">{title}</h2>
@@ -28,8 +28,12 @@ const JobListCard = (job) => {
             <small className="mb-2">{address}</small>
           </div>
           <div className="flex gap-3 items-center">
-            <p className="text-secondary text-sm font-bold">{level}</p>
-            <p className="text-secondary text-sm font-bold">{type}</p>
+            <div className="bg-pink-100 text-white-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-pink-200 dark:text-pink-900">
+              {level}
+            </div>
+            <div className="bg-purple-100 text-white-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-900">
+              {type}
+            </div>
           </div>
         </div>
       </div>
