c33f324643eb34944b2aff323d6a046ca7b88820
Ahasan Habib Sany <sany.baust@gmail.com>
2022-10-29 00:18:55 +0600

    category done

--- src/pages/homePage/homePageComponents/CategorieCart.js
+++ src/pages/homePage/homePageComponents/CategorieCart.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { FaDigitalTachograph } from "react-icons/fa";
 
 const CategorieCart = (categorie) => {
-  const { name, availableJob, icon } = categorie.categorie;
+  const { category, availableJob } = categorie.categorie;
   return (
     <div className="flex justify-start items-center bg-base-100 shadow gap-4 px-5 py-5 cursor-pointer hover:-translate-y-1 duration-700 hover:shadow-lg border-white border hover:border-primary">
-      <div className="bg-primary p-4 text-white text-2xl">{icon}</div>
+      <div className="bg-primary p-4 text-white text-2xl"><FaDigitalTachograph /></div>
       <div className="">
-        <h2 className="font-bold text-xl text-accent">{name}</h2>
-        <p className="text-primary font-bold">{availableJob} Jobs</p>
+        <h2 className="font-bold text-xl text-accent">{category}</h2>
+        {/* <p className="text-primary font-bold">{availableJob} Jobs</p> */}
       </div>
     </div>
   );
--- src/pages/homePage/homePageComponents/Categories.js
+++ src/pages/homePage/homePageComponents/Categories.js
@@ -1,64 +1,68 @@
 import React from "react";
-import { CgWebsite } from "react-icons/cg";
-import { BsMicrosoft } from "react-icons/bs";
-import { FcManager } from "react-icons/fc";
-import { TbDatabaseOff } from "react-icons/tb";
-import { AiFillAndroid } from "react-icons/ai";
-import { FaDigitalTachograph } from "react-icons/fa";
-import { HiPhotograph, HiOutlineDesktopComputer } from "react-icons/hi";
+// import { CgWebsite } from "react-icons/cg";
+// import { BsMicrosoft } from "react-icons/bs";
+// import { FcManager } from "react-icons/fc";
+// import { TbDatabaseOff } from "react-icons/tb";
+// import { AiFillAndroid } from "react-icons/ai";
+// import { FaDigitalTachograph } from "react-icons/fa";
+// import { HiPhotograph, HiOutlineDesktopComputer } from "react-icons/hi";
 import CategorieCart from "./CategorieCart";
+import { useGetCategoryQuery } from "../../../features/category/categoryApi";
 
 const Categories = () => {
-  const categories = [
-    {
-      _id: 1,
-      name: "Web & Software Dev",
-      availableJob: 203,
-      icon: <BsMicrosoft />,
-    },
-    {
-      _id: 2,
-      name: "Sales & Marketing",
-      availableJob: 303,
-      icon: <HiOutlineDesktopComputer />,
-    },
-    {
-      _id: 3,
-      name: "Graphics",
-      availableJob: 120,
-      icon: <HiPhotograph />,
-    },
-    {
-      _id: 4,
-      name: "UI/UX Designer",
-      availableJob: 784,
-      icon: <CgWebsite />,
-    },
-    {
-      _id: 5,
-      name: "Senior Manager",
-      availableJob: 233,
-      icon: <FcManager />,
-    },
-    {
-      _id: 6,
-      name: "SEO",
-      availableJob: 78,
-      icon: <TbDatabaseOff />,
-    },
-    {
-      _id: 7,
-      name: "Android Developer",
-      availableJob: 70,
-      icon: <AiFillAndroid />,
-    },
-    {
-      _id: 8,
-      name: "Digital Marketer",
-      availableJob: 23,
-      icon: <FaDigitalTachograph />,
-    },
-  ];
+
+  const {data ,isLoading,error,} = useGetCategoryQuery();
+  const categories = data?.all_category
+  // const categories = [
+  //   {
+  //     _id: 1,
+  //     name: "Web & Software Dev",
+  //     availableJob: 203,
+  //     icon: <BsMicrosoft />,
+  //   },
+  //   {
+  //     _id: 2,
+  //     name: "Sales & Marketing",
+  //     availableJob: 303,
+  //     icon: <HiOutlineDesktopComputer />,
+  //   },
+  //   {
+  //     _id: 3,
+  //     name: "Graphics",
+  //     availableJob: 120,
+  //     icon: <HiPhotograph />,
+  //   },
+  //   {
+  //     _id: 4,
+  //     name: "UI/UX Designer",
+  //     availableJob: 784,
+  //     icon: <CgWebsite />,
+  //   },
+  //   {
+  //     _id: 5,
+  //     name: "Senior Manager",
+  //     availableJob: 233,
+  //     icon: <FcManager />,
+  //   },
+  //   {
+  //     _id: 6,
+  //     name: "SEO",
+  //     availableJob: 78,
+  //     icon: <TbDatabaseOff />,
+  //   },
+  //   {
+  //     _id: 7,
+  //     name: "Android Developer",
+  //     availableJob: 70,
+  //     icon: <AiFillAndroid />,
+  //   },
+  //   {
+  //     _id: 8,
+  //     name: "Digital Marketer",
+  //     availableJob: 23,
+  //     icon: <FaDigitalTachograph />,
+  //   },
+  // ];
   return (
     <section className="py-16 bg-secondary">
       <div className="container mx-auto px-5">
@@ -71,7 +75,7 @@ const Categories = () => {
           </p>
         </div>
         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
-          {categories.map((categorie) => (
+          {categories?.map((categorie) => (
             <CategorieCart
               key={categorie._id}
               categorie={categorie}
