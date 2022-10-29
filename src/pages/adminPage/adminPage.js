4d908b6e361e7063e13f24a70631f1b08b348f05
nayim-hossen <nayimh2017@gmail.com>
2022-10-29 18:27:41 +0600

    add some style on dashbord

--- src/pages/adminPage/adminPage.js
+++ src/pages/adminPage/adminPage.js
@@ -7,9 +7,28 @@ const AdminPage = () => {
   const location = useLocation().pathname;
   return (
     <>
-      <div className="drawer drawer-mobile h-fit border-y-1 border-accent">
+      <div className="drawer drawer-mobile border-t-1 border-accent">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content p-4">
+          {/* mobile sidebar menu open */}
+          <div className="flex justify-between items-center mb-2">
+            <h2 className="text-2xl md:text-3xl font-bold text-primary">
+              Admin Dashboard
+            </h2>
+            <label htmlFor="my-drawer-2" className="lg:hidden">
+              <svg
+                className="swap-off"
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 512 512"
+              >
+                <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+              </svg>
+            </label>
+          </div>
+          {/* mobile sidebar open menu end*/}
+
           {/* <!-- Page content here --> */}
           <Outlet />
           {/* {(location === '/dashboard' || location === '/dashboard/') && (
--- src/pages/recruiterPage/RecruiterPage.js
+++ src/pages/recruiterPage/RecruiterPage.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { AiOutlineMenuUnfold } from "react-icons/ai";
 import RecruiterSidebar from "./recruiterComponents/RecruiterSidebar";
 
 const RecruiterPage = () => {
   return (
-    <div className="drawer drawer-mobile border-y-2 border-accent">
+    <div className="drawer drawer-mobile border-t-1 border-accent">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content p-4">
-        <div>
-          <h2>Recruiter Dashboard</h2>
-          <AiOutlineMenuUnfold size={20} />
+        {/* mobile sidebar menu open */}
+        <div className="flex justify-between items-center mb-2">
+          <h2 className="text-2xl md:text-3xl font-bold text-primary">
+            Recruiter Dashboard
+          </h2>
+          <label htmlFor="my-drawer-2" className="lg:hidden">
+            <svg
+              className="swap-off"
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 512 512"
+            >
+              <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+            </svg>
+          </label>
         </div>
+        {/* mobile sidebar open menu end*/}
+
         {/* <!-- Page content here --> */}
         <Outlet />
       </div>
